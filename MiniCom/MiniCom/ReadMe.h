7553e3ec540676592bd757b3e2edcf7430dab4b9
“poplarsunshine” <wlpych@126.com>
2014-08-14 23:08:33 +0800

    add ReadMe.h

---
+++ MiniCom/MiniCom/ReadMe.h
@@ -0,0 +1,97 @@
+//
+//  ReadMe.h
+//  MiniCom
+//
+//  Created by wlp on 14-8-14.
+//  Copyright (c) 2014年 wanglipeng. All rights reserved.
+//
+
+#ifndef MiniCom_ReadMe_h
+#define MiniCom_ReadMe_h
+
+/*
+ 组织目录说明
+ 1、LIb（第三方类库）
+ GTMBase64
+ 封装了关于base64的方法
+ 
+ slib及rsa
+ 封装了rsa解密的方法
+ 
+ tcp
+ 长连接
+ 
+ SDWebImage
+ 图片缓存封装
+ 
+ ASI
+ http封装
+ 
+ FMDB
+ 数据库封装
+ 
+ HUD
+ 遮罩封装
+ 
+ JSONKit
+ json库
+ 
+ Reachability
+ 网络监测类库
+ 
+ 2、class
+ common
+ 工具类
+ 
+ DataBase
+ 数据管理类包括数据库
+ 
+ Manager
+ 账户管理
+ 音频播放管理
+ 广场信息管理类
+ 群组信息管理类
+ 
+ MyHttp
+ 封装了针对本项目的关于网络的类
+ 
+ Model
+ 各模块的数据类型
+ 
+ View
+ 各个模块的自定义VIew
+ 
+ ViewControl
+ 各个视图控制器
+ Main：主界面（子view广场、群组、我的、发送消息）
+ Loading：loading动画页
+ Login：登录模块
+ Chat:聊天模块
+ SquareMess：广场详情
+ UserInfo：好友信息
+ GroupInfo：群组信息
+ Own：
+ MoreFriend（找到更多密友）
+ AddFriend（添加好友）
+ NewFriend（好友请求）
+ CitcleSet（好友分组设置）
+ Group：
+ GroupManager（群组管理）
+ GroupMemberSet（添加群成员）
+ GroupNewLocal(创建群组设置位置)
+ GroupCreatSuccess(创建群组成功后，确认页面）
+ AccountVC
+ MyInfo（我的名片）
+ MyInfoCHange（修改个人信息）
+ MyInfoCHangePassword（修改密码）
+ EditMess
+ SendMessage（发送广场信息）
+ ChoosePhoto（选择图片）
+ RecordVoice（录制音频）
+ 
+ 3、resources
+ 资源文件
+
+ */
+
+#endif
